e9a2fefaea243f947d0891b1fbc296e737bba912
MadTBDiv <Dmcoding04>
2020-06-26 19:00:53 -0400

    Created fizzbuzz test.

---
+++ exercises/fundamentals/test/test_fizzbuzz.py
@@ -0,0 +1,12 @@
+from exercises.fundamentals.src.fizzbuzz import fizzbuzz
+
+
+def test_fizzbuzz():
+    output = fizzbuzz(9)
+    assert output == 'fizz'
+
+    output = fizzbuzz(10)
+    assert output == 'buzz'
+
+    output = fizzbuzz(15)
+    assert output == 'fizzbuzz'
