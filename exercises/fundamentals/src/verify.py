0d371c2bbd8961a31686211b844e78d900cee89a
Brandon <b.mazey@nyu.edu>
2020-06-26 18:03:38 -0400

    adding a run config

--- exercises/fundamentals/src/verify.py
+++ exercises/fundamentals/src/verify.py
@@ -2,3 +2,11 @@
 
 def verify():
     return "congratulations! don't forget to email your github username to b.mazey@nyu.edu!"
+
+
+def console():
+    print("this is a print-to-console test")
+
+
+if __name__ == '__main__':
+    console()
