30491c0405d8bfb8d1016bf5294dd409f4551de2
MadTBDiv <Dmcoding04>
2020-07-14 17:58:53 -0400

    Fizzloop completed

--- exercises/fundamentals/src/verify.py
+++ exercises/fundamentals/src/verify.py
@@ -4,9 +4,28 @@ def verify():
     return "congratulations! don't forget to email your github username to b.mazey@nyu.edu!"
 
 
-def console():
-    print("this is a print-to-console test")
+def console(x):
+    # for c in x:
+       # if c == 'a' or c == 'y':
+           # print(c)
+    y = x + '!'
+    return y
+
+
+def fizzloop(x):
+    for i in range(x):
+        print('value of i is: ' + str(i))
+        if i % 3 == 0:
+            print('fizz')
+        if i % 5 == 0:
+            print('buzz')
 
 
 if __name__ == '__main__':
-    console()
+    # z = console('Happy Tuesday')
+    # print(z)
+    # z = console('Tomorrow is Wednesday')
+    # print(z)
+    # console('Day after is Thursday')
+    # console(str(2) + 'test')
+    fizzloop(10)
