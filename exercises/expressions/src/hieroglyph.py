c0128811e43556660626fc6410b8c7fea916a19b
Brandon <b.mazey@nyu.edu>
2020-08-03 21:31:45 -0400

    added hieroglyphs!

---
+++ exercises/expressions/src/hieroglyph.py
@@ -0,0 +1,23 @@
+import re
+
+
+# https://www.tutorialspoint.com/python3/python_reg_expressions.htm
+# example: return re.match(r'my-expression', s)
+class Hieroglyph(object):
+    def __init__(self):
+        return
+
+    @staticmethod
+    def worship_sacred_cats(s):
+        # FIXME!
+        return True
+
+    @staticmethod
+    def alphanumeric_glyph(s):
+        # FIXME!
+        return True
+
+    @staticmethod
+    def steal_crystal_skull(s):
+        # FIXME!
+        return s
---
+++ exercises/expressions/test/test_hieroglyph.py
@@ -0,0 +1,27 @@
+from exercises.expressions.src.hieroglyph import Hieroglyph
+
+
+def test_sacred_cat():
+    # all cats are sacred!
+    assert not Hieroglyph.worship_sacred_cats('dog dog dog')
+    assert Hieroglyph.worship_sacred_cats('cat')
+    assert Hieroglyph.worship_sacred_cats('dog cat dog dog')
+    assert Hieroglyph.worship_sacred_cats('scarab cat scarab cat')
+    assert Hieroglyph.worship_sacred_cats('cat cat ')
+
+
+def test_alphanumeric_glyph():
+    assert Hieroglyph.alphanumeric_glyph('wxyd67891')
+    assert not Hieroglyph.alphanumeric_glyph('ad143')
+    assert not Hieroglyph.alphanumeric_glyph('34561')
+    assert not Hieroglyph.alphanumeric_glyph('sand pyramid')
+    assert Hieroglyph.alphanumeric_glyph('gold2134598')
+    assert Hieroglyph.alphanumeric_glyph('sand104923')
+    assert not Hieroglyph.alphanumeric_glyph('143sand')
+
+
+def test_steal_crystal_skull():
+    assert Hieroglyph.steal_crystal_skull('crystal skull') == 'crystal idol'
+    assert Hieroglyph.steal_crystal_skull('skull crystal skull') == 'idol crystal idol'
+    assert Hieroglyph.steal_crystal_skull('crystal boulder') == 'crystal boulder'
+    assert Hieroglyph.steal_crystal_skull('skull skull boulder skull') == 'idol idol boulder idol'
