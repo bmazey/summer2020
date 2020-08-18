86bfc0d596564e5c3dbe493b33fed7f00ef6cccb
MadTBDiv <Dmcoding04>
2020-08-18 19:00:33 -0400

    Rabin-Karp Draft 1

---
+++ exercises/algorithms/src/rabin_karp.py
@@ -0,0 +1,37 @@
+
+
+def detect_plagiarism(source, submission):
+
+    q = 101  # prime number
+    d = 256  # number of possible input characters
+    n = len(source)  # length of source
+    m = len(submission)  # length of submission
+    p = 0  # hash value for submisson
+    t = 0  # hash value for source
+    h = 1  # hash of the index
+
+    # TODO - explain me
+    for i in range(m - 1):
+        h = (h * d) % q
+
+    # calculating the hash for the first window of text
+    for i in range(m):
+        p = (d * p + ord(submission[i])) % q
+        t = (d * t + ord(source[i])) % q
+
+    # slide the text over the pattern one by one
+    for i in range(n - m + 1):
+        # check to see if hashes are the same
+        if p == t:
+            return True
+
+        # calculate next hash value for t
+        if i < m - n:
+            t = (d * (t - ord(source[i]) * h) + ord(source[i + m])) % q
+
+            # We might get negative values of t, converting it to
+            # positive
+            if t < 0:
+                t += q
+
+    return False
